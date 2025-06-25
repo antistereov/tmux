ef0ca53469f9f3ba527342f67651fadac07101fd
Andre Antimonov <andre.antimonov@systema.com>
2025-06-25 12:39:03 +0200

    feat(installation): add installation script and README

---
+++ README.md
@@ -0,0 +1,16 @@
+# Installation
+
+1. Make sure that `tmux` is installed on your system.
+
+2. Please clone this repository to your `~/.config/tmux` directory:
+
+    ```bash
+    mkdir -p ~/.config/tmux
+    git clone git@github.com:antistereov/tmux.git ~/.config/tmux
+    ```
+
+3. To install this custom configuration, run:
+
+    ```bash
+    install.sh
+    ```
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Installing Catppuccin..."
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
+echo "Installing tmux-cpu..."
+mkdir -p ~/.config/tmux/plugins/tmux-cpu
+git clone https://github.com/tmux-plugins/tmux-cpu.git ~/.config/tmux/plugins/tmux-cpu
+
+echo "Installing tmux-battery..."
+mkdir -p ~/.config/tmux/plugins/tmux-battery
+git clone https://github.com/tmux-plugins/tmux-battery.git ~/.config/tmux/plugins/tmux-cpu
+
+echo "Applying custom configuration..."
+tmux source ~/.config/tmux/tmux.conf
